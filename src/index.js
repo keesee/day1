881ba8e407dc1434be9d219beb53aa887b803d6d
Jimmy Keesee <jimmy@keesee.net>
2019-09-21 11:58:55 +0800

    added notes

--- src/index.js
+++ src/index.js
@@ -13,3 +13,28 @@ if (( testAddNumbers > 5 ) && ( testAddNumbers < 10 )){
 } else {
     console.log("The number is not big enough!")
 }
+
+
+for (ii = 0; ii <= 100; ii = ii + 1) {
+    var current = ii
+    console.log(current)
+}
+
+
+
+
+//////////////////////// Homework /////
+// Functions ////// 
+var sampleFunction = function(x,y) {
+
+}
+// flow control 
+if (true) {
+
+} else {
+
+}
+// for loops
+for (ii = 0; ii < 100; ii++) {
+
+}
